d0509d1238fab029e5c4cb6b9eead0a913b12e46
MrGlox <contact@morgan-leroux.com>
2016-10-17 14:48:41 +0200

    rename and decomposition

--- js/audioData.js
+++ js/audioData.js
@@ -10,24 +10,6 @@
 /***
  * INIT BASES
 ***/
-window.requestAnimFrame = (function (){
-	return window.requestAnimationFrame        ||
-		window.webkitRequestAnimationFrame     ||
-		window.mozRequestAnimationFrame        ||
-		function(callback, element){
-			window.setTimeout(callback, 1000 / 60);
-		};
-})();
-
-window.cancelAnimationFrame = (function () {
-    return window.cancelAnimationFrame 	||
-		window.webkitCancelAnimationFrame 	||
-		window.mozCancelAnimationFrame 		||
-		function (timPtr) {
-        	window.clearTimeout(timPtr);
-    	};
-})();
-
 window.AudioContext = (function (){
 	return window.AudioContext || window.mozAudioContext;
 })();
@@ -89,9 +71,9 @@ var setupAudioNodes = function () {
 	sourceNode = audioContext.createBufferSource();
 	analyserNode = audioContext.createAnalyser();
 	analyserNode.fftSize = fftSize;
-	analyser.smoothingTimeConstant = smoothingTimeConstant;
-	analyser.minDecibels = minDecibels;
-	analyser.maxDecibels = maxDecibels;
+	analyserNode.smoothingTimeConstant = smoothingTimeConstant;
+	analyserNode.minDecibels = minDecibels;
+	analyserNode.maxDecibels = maxDecibels;
 	gainNode = audioContext.createGain();
 	javascriptNode = audioContext.createScriptProcessor(sampleSize, 1, 1);
 	amplitudeArray = new Uint8Array(analyserNode.frequencyBinCount);
@@ -196,163 +178,3 @@ var getOffset = function (minAmplitude){
 
 	return offset;
 };
-
-/***
- * INIT CANVAS VARS
-***/
-var canvas,
-	ctx,
-	cx,
-	cy;
-
-var resize = function () {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-};
-
-var clearCanvas = function () {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-};
-
-var drawTimeDomain = function () {
-	clearCanvas();
-
-	loader();
-
-	requestAnimFrame(drawTimeDomain);
-};
-
-/***
- * CANVAS PATTERNS
-***/
-var loader = function (){
-	var roundLoader = roundDash(cx, cy, cy/2, 5, 160);
-};
-
-/***
- * CANVAS PATHS
-***/
-var roundDash = function (cx, cy, r, height, number){
-	var lines = [];
-	var a = (Math.PI*2)/number;
-
-	for(var i = 0; i<number; i++){
-		var x1 = cx + (r * Math.cos(a*i));
-		var y1 = cy + (r * Math.sin(a*i));
-
-		var x2 = cx + ((r + 5) * Math.cos(a*i));
-		var y2 = cy + ((r + 5) * Math.sin(a*i));
-
-		lines.push({x1: x1, y1: y1, x2: x2, y2: y2});
-
-		ctx.beginPath();
-
-		ctx.lineWidth = 1;
-		ctx.strokeStyle = 'rgb(0, 0, 0)';
-
-		ctx.moveTo(x1,y1);
-		ctx.lineTo(x2,y2);
-
-		ctx.stroke();
-	}
-	return lines;
-};
-
-var roundPulse = function (cx, cy, r, lineWidth){
-	ctx.save();
-	ctx.beginPath();
-	ctx.arc(cx, cy, r * amplitudeArray/100, 0, 2 * Math.PI, false);
-	ctx.lineWidth = lineWidth;
-	ctx.strokeStyle = 'rgb(0, 0, 0)';
-	ctx.stroke();
-	ctx.restore();
-};
-
-var roundFrequency = function (cx, cy, r){
-	ctx.save();
-
-	var a = (Math.PI*2)/amplitudeArray.length;
-	var x = cx + (r * Math.cos(a));
-	var y = cy + (r * Math.sin(a));
-
-	ctx.beginPath();
-	ctx.lineJoin="round";
-
-	ctx.lineWidth = 5;
-	ctx.strokeStyle = 'rgb(0, 0, 0)';
-
-	getMax();
-	getMin();
-
-	for (var i = 0; i < amplitudeArray.length; i++) {
-		if(amplitudeArray[i]){
-
-		}
-	}
-
-	ctx.closePath();
-	ctx.stroke();
-	ctx.restore();
-};
-
-var roundDistort = function (cx, cy, r, minAmplitude, maxAmplitude){
-	ctx.save();
-
-	var a = (Math.PI*2)/4;
-	var x = cx + (r * Math.cos(a));
-	var y = cy + (r * Math.sin(a));
-
-	ctx.beginPath();
-	ctx.lineJoin="round";
-
-	ctx.lineWidth = 5;
-	ctx.strokeStyle = 'rgb(0, 0, 0)';
-
-	for (var i = 0; i < amplitudeArray.length; i++) {
-		var v = amplitudeArray[i] / 2;
-
-		x = cx + ((r+v) * Math.cos(a*i));
-		y = cy + ((r+v) * Math.sin(a*i));
-
-		ctx.lineTo(x, y);
-	}
-
-	ctx.closePath();
-	ctx.stroke();
-	ctx.restore();
-};
-
-
-/***
- * IIFE
-***/
-(function() {
-	canvas = document.getElementById('audioVisualizer');
-	ctx = canvas.getContext("2d");
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-
-	cx = canvas.width/2;
-	cy = canvas.height/2;
-
-	initLoader();
-	loader();
-	window.onresize = resize;
-
-	try {
-		audioContext = new AudioContext();
-	} catch(e) {
-		alert('Web Audio API is not supported in this browser');
-	}
-
-	setupAudioNodes();
-	javascriptNode.onaudioprocess = function () {
-		analyserNode.getByteTimeDomainData(amplitudeArray);
-	};
-
-	if(audioData === null) {
-		loadSound(audioUrl);
-	}
-
-	requestAnimFrame(drawTimeDomain);
-})();
